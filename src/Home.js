7c3ccbb40a9951f9a8e6d9ff17e5f2e2fb19ca78
Harsh Ghodkar <harshghodkar@gmail.com>
2021-10-30 16:12:36 +0530

    CRUD Done :star_struck:

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import { Box, Button } from "@chakra-ui/react";
 import { useMoralis } from "react-moralis";
+import { ErrorBox } from "./Error";
 
 export const Home = () => {
   const { Moralis, isUserUpdating } = useMoralis();
@@ -7,8 +8,10 @@ export const Home = () => {
   const sendData = () => {
     const API_DATABASE = Moralis.Object.extend("API_DATABASE");
     const apiDatabase = new API_DATABASE();
+    apiDatabase.set("User", Moralis.User.current());
     apiDatabase.set("Name", "My Cool API");
     apiDatabase.set("Description", "This is a cool API");
+    apiDatabase.set("URL", "https://www.google.com");
     apiDatabase.save().then(() => {
       console.log("Successfully saved.");
     });
@@ -21,6 +24,66 @@ export const Home = () => {
       console.log(results);
     });
   };
+
+  const updateData = () => {
+    const API_DATABASE = Moralis.Object.extend("API_DATABASE");
+    const query = new Moralis.Query(API_DATABASE);
+    query.equalTo("Name", "My Cool API");
+    query.equalTo("User", Moralis.User.current());
+    query.find().then((results) => {
+      const apiDatabase = results[0];
+      apiDatabase.set("Description", "This is a not a cool API");
+      apiDatabase.save().then(() => {
+        console.log("Successfully saved.");
+      });
+    });
+  };
+
+  const deleteData = () => {
+    const API_DATABASE = Moralis.Object.extend("API_DATABASE");
+    const query = new Moralis.Query(API_DATABASE);
+    query.equalTo("Name", "My Cool API");
+    query.find().then((results) => {
+      const apiDatabase = results[0];
+      if (apiDatabase)
+        apiDatabase.destroy().then(() => {
+          console.log("Successfully deleted.");
+        });
+      else alert("No Records found");
+    });
+  };
+
+  const getUserSpecificData = () => {
+    const API_DATABASE = Moralis.Object.extend("API_DATABASE");
+    const query = new Moralis.Query(API_DATABASE);
+    query.equalTo("Name", "My Cool API");
+    query.equalTo("User", Moralis.User.current());
+    query.find().then((results) => {
+      console.log(results);
+    });
+  };
+
+  const deleteSpecificData = () => {
+    const API_DATABASE = Moralis.Object.extend("API_DATABASE");
+    const query = new Moralis.Query(API_DATABASE);
+    query.equalTo("Name", "My Cool API");
+    query.equalTo("User", Moralis.User.current());
+
+    try {
+      query.find().then((results) => {
+        const apiDatabase = results[0];
+        if (apiDatabase)
+          apiDatabase.destroy().then(() => {
+            console.log("Successfully deleted.");
+          });
+        else alert("No data found");
+      });
+    } catch (e) {
+      <ErrorBox title={"Not able to delete"} message={e} />;
+      console.log(e);
+    }
+  };
+
   return (
     <Box>
       Home
@@ -31,6 +94,22 @@ export const Home = () => {
       <Button onClick={getData} isLoading={isUserUpdating}>
         {" "}
         Get API Data
+      </Button>
+      <Button onClick={updateData} isLoading={isUserUpdating}>
+        {" "}
+        Update API Data
+      </Button>
+      <Button onClick={deleteData} isLoading={isUserUpdating}>
+        {" "}
+        Delete API Data
+      </Button>
+      <Button onClick={getUserSpecificData} isLoading={isUserUpdating}>
+        {" "}
+        Get User Specific Data
+      </Button>
+      <Button onClick={deleteSpecificData} isLoading={isUserUpdating}>
+        {" "}
+        Delete User Specific Data
       </Button>
     </Box>
   );
