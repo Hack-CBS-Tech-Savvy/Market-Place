3b161888a199e350572e0c6ed9582d94353dbcbf
Harsh Ghodkar <harshghodkar@gmail.com>
2021-10-30 13:59:47 +0530

    Database connection worked :tada:

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,25 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { useMoralis } from "react-moralis";
 
 export const Home = () => {
-  return <Box>Home</Box>;
+  const { Moralis, isUserUpdating } = useMoralis();
+
+  const sendData = () => {
+    const API_DATABASE = Moralis.Object.extend("API_DATABASE");
+    const apiDatabase = new API_DATABASE();
+    apiDatabase.set("Name", "My Cool API");
+    apiDatabase.set("Description", "This is a cool API");
+    apiDatabase.save().then(() => {
+      console.log("Successfully saved.");
+    });
+  };
+  return (
+    <Box>
+      Home
+      <Button onClick={sendData} isLoading={isUserUpdating}>
+        {" "}
+        Send API Data
+      </Button>
+    </Box>
+  );
 };
